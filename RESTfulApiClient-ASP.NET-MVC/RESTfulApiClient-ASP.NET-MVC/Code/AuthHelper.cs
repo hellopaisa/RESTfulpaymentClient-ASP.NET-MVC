0f858d3165da1d65a5de7c5907e6b8524ff2576b
poo5zan <poo5zan@gmail.com>
2014-09-18 16:33:48 +0545

    dotnetopenauth library

---
+++ RESTfulApiClient-ASP.NET-MVC/RESTfulApiClient-ASP.NET-MVC/Code/AuthHelper.cs
@@ -0,0 +1,31 @@
+﻿using DotNetOpenAuth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTfulApiClient_ASP.NET_MVC.Code
+{
+    public class AuthHelper
+    {
+        public static WebServerClient CreateClient()
+        {
+            var desc = GetAuthServerDescription();
+            var client = new WebServerClient(desc, clientIdentifier: "PUJAN_ID");
+            client.ClientCredentialApplicator = ClientCredentialApplicator.PostParameter("PUJAN_SECRET");
+
+            return client;
+        }
+
+        private static AuthorizationServerDescription GetAuthServerDescription()
+        {
+            //throw new NotImplementedException();
+            var authServerDesc = new AuthorizationServerDescription();
+            authServerDesc.AuthorizationEndpoint = new Uri(@"https://localhost:44302/OAuth/Auth");
+            authServerDesc.TokenEndpoint = new Uri(@"https://localhost:44302/OAuth/Token");
+            authServerDesc.ProtocolVersion = ProtocolVersion.V20;
+            return authServerDesc;
+
+        }
+    }
+}
---
+++ RESTfulApiClient-ASP.NET-MVC/RESTfulApiClient-ASP.NET-MVC/Code/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace RESTfulApiClient_ASP.NET_MVC.Code
+{
+    public static class Helper
+    {
+        public static void SetCertificatePolicy()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += RemoteCertificateValidate;
+        }
+
+        private static bool RemoteCertificateValidate(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
